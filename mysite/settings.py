7f0e8532b9197c8b38b396534733c1ce18218043
Alex <oleksandr.tereshchenko@nure.ua>
2020-03-21 19:07:06 +0200

    Fix and add content

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=f_r)p5e#ceq*5*@v!wj#7t4jec96c5&3%p!+&%k@mq49pnu%_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
